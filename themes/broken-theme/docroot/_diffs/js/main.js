1efceae3a1bb76751798b3e0b880cc32080a0ee2
JORDAN_KILMER <JORDAN.KILMER@LIFERAY.COM>
2014-06-17 12:17:00 -0700

    Broken theme fixes

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -1,5 +1,5 @@
 AUI().ready(
-	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal',
+	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal', 'aui-modal', 'aui-node', 'aui-io-request',
 	function(A) {
 		var navigation = A.one('#navigation');
 
@@ -7,15 +7,15 @@ AUI().ready(
 			navigation.plug(Liferay.NavigationInteraction);
 		}
 
-		var siteBreadcrumb = A.one('#breadcrumbs');
+		var siteBreadcrumbs = A.one('#breadcrumbs');
 
 		if (siteBreadcrumbs) {
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
 
-		A.getBody().delegate('click', eventHanler, 'a.logo');
+		A.getBody().delegate('click', eventHandler, 'a.logo');
 
-		var eventHandler function(event) {
+		var eventHandler = function(event) {
 			event.preventDefault();
 			alert(event.currentTarget.attr('title'));
 		}
@@ -24,6 +24,67 @@ AUI().ready(
 
 		if (signIn && signIn.getData('redirect') !== 'true') {
 			signIn.plug(Liferay.SignInModal);
+		}
+
+		var footer = A.one('p.powered-by a');
+
+		if (footer) {
+			var liferayPopup = '<iframe src="http://www.liferay.com" style="height: 100%; width: 100%; z-index: 999;"></iframe>';
+
+			footer.on('click', 
+				function(event) {
+					event.preventDefault();
+
+					var windowHeight = window.innerHeight,
+						windowWidth = window.innerWidth,
+						height = (windowHeight * 0.75),
+						width = (windowWidth * 0.75);
+
+					var modal = new A.Modal(
+						{
+						bodyContent: liferayPopup,
+						centered: true,
+						height: height,
+						render: '#popup-modal',
+						width: width
+						}
+					).render();
+				}
+			);
+		}
+
+		if (themeDisplay.isSignedIn() !== true) {
+			var signinBreadcrumb = A.one('.breadcrumb li a');
+
+			if (signinBreadcrumb) {
+
+				signinBreadcrumb.on('click', 
+					function(event) {
+						event.preventDefault();
+
+						var signIn = A.io.request(
+							'http://localhost:8080/c/portal/login',
+							{
+								on: {
+									'success': function() {
+										var signinPopup = this.get('responseData');
+
+										var modal = new A.Modal(
+											{
+											bodyContent: signinPopup,
+											centered: true,
+											height: 750,
+											render: '#breadcrumb-modal',
+											width: 750
+											}
+										).render();
+									}
+								}
+							}
+						);
+					}
+				);
+			}
 		}
 	}
 );
